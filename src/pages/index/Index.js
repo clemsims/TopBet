a1976006175fc2ec6b9f7d2448e9ef5621322eb3
antoine_bossan <antoine9589@gmail.com>
2023-02-26 06:55:03 +0000

    text added at the buttom

--- src/pages/index/Index.js
+++ src/pages/index/Index.js
@@ -19,7 +19,7 @@ export default class Index extends Component {
   };
 
   handleBestRates = () => {
-    this.props.history.push("/best-quotations");
+    this.props.history.push("/best-rates");
   };
 
   render() {
@@ -27,7 +27,12 @@ export default class Index extends Component {
       <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
           <a className="navbar-brand h4 mb-0" href="#">
-            <img src={logo} alt="TopBet Logo" className="img-fluid logo" style={{maxWidth: "80px"}} />
+            <img
+              src={logo}
+              alt="TopBet Logo"
+              className="img-fluid logo"
+              style={{ maxWidth: "80px" }}
+            />
           </a>
           <button
             className="navbar-toggler"
@@ -36,7 +41,7 @@ export default class Index extends Component {
             data-target="#navbar-side"
             aria-controls="navbar-side"
             aria-expanded="false"
-            aria-label="Abrir navegação"
+            aria-label="Open navigation"
           >
             <span className="navbar-toggler-icon" />
           </button>
@@ -46,14 +51,6 @@ export default class Index extends Component {
               <li className="nav-item">
                 <a className="nav-link link-pointer" onClick={console.log("test")}>
                   Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link link-pointer "
-                  onClick={console.log("test")}
-                >
-                  Equipe
                 </a>
               </li>
               <li className="nav-item">
@@ -78,42 +75,41 @@ export default class Index extends Component {
           </div>
         </nav>
 
-        <div
-          id="carouselExampleIndicators"
-          className="carousel slide"
-          data-ride="carousel"
-        >
-          <ol class="carousel-indicators">
-            <li
-              data-target="#carouselExampleIndicators"
-              data-slide-to="0"
-              class="active"
-            />
+        <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+          <ol className="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active" />
             <li data-target="#carouselExampleIndicators" data-slide-to="1" />
           </ol>
-          <div id="carouselExampleIndicators" class="carousel-inner">
-            <div class="carousel-item active">
-              <img
-                class="d-block img-fluid w-100 mx-auto"
-                src={img1}
-                alt="First slide"
-              />
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img className="d-block img-fluid w-100 mx-auto" src={img1} alt="First slide" />
             </div>
-            <div class="carousel-item">
-              <img
-                class="d-block img-fluid w-100 mx-auto"
-                src={img3}
-                alt="Third slide"
-              />
+            <div className="carousel-item">
+              <img className="d-block img-fluid w-100 mx-auto" src={img3} alt="Third slide" />
             </div>
           </div>
           <a
-              class="carousel-control-prev"
-              href="#carouselExampleIndicators"
-              role="button"
-              data-slide="prev"
-            >
-              <span class="carousel-control-prev-icon" aria-hidden="true" />
-              <span class="sr-only">Anterior</span>
-            </a>
-</div> </div> ) }}
+            className="carousel-control-prev"
+            href="#carouselExampleIndicators"
+            role="button"
+            data-slide="prev"
+          >
+            <span className="carousel-control-prev-icon" aria-hidden="true" />
+            <span className="sr-only">Previous</span>
+          </a>
+        </div>
+
+        <div className="buttom-text">
+          At Top Bet, we're committed to promoting responsible and ethical sports betting practices. <br />
+          That's why we offer rewards for watching ads, rather than requiring users to bet money.<br />
+          This ensures that users can enjoy the thrill of sports betting without taking unnecessary risks <br />
+          or engaging in behavior that is harmful to themselves or others.<br />
+          Our rewards program is designed to be fair, transparent, and easy to use. <br />
+          Simply watch ads and earn rewards that you can use to place bets on your favorite sports teams and events.<br />
+          We provide clear and upfront information on the terms and conditions of our rewards program,<br />
+          so you can make informed decisions about your betting and avoid any potential risks or issues.
+        </div>
+      </div>
+    );
+  }
+}
