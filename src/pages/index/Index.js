3018a13c5723118f8431da5cfb7e70f3ed9b13dc
antoine_bossan <antoine9589@gmail.com>
2023-03-07 08:17:17 +0000

    translate page not found

--- src/pages/index/Index.js
+++ src/pages/index/Index.js
@@ -1,4 +1,3 @@
-
 import React, { Component } from "react";
 
 import $ from "jquery";
@@ -7,27 +6,26 @@ import "./styles.css";
 
 //import logo from '../index/images/TOPBET.png';
 
-
 // Define an array of upcoming events
 const upcomingEvents = [
   {
     id: 1,
     title: "Manchester United vs. Liverpool",
     date: "March 5, 2023",
-    time: "9:00 AM EST",
+    time: "9:00 AM EST"
   },
   {
     id: 2,
     title: "Los Angeles Lakers vs. Brooklyn Nets",
     date: "March 6, 2023",
-    time: "8:30 PM EST",
+    time: "8:30 PM EST"
   },
   {
     id: 3,
     title: "Boston Red Sox vs. New York Yankees",
     date: "March 7, 2023",
-    time: "1:05 PM EST",
-  },
+    time: "1:05 PM EST"
+  }
 ];
 
 // Define an array of open bets
@@ -36,84 +34,78 @@ const openBets = [
     id: 1,
     team: "Manchester United",
     odds: 2.0,
-    stake: 10,
+    stake: 10
   },
   {
     id: 2,
     team: "Los Angeles Lakers",
     odds: 1.5,
-    stake: 5,
-  },
+    stake: 5
+  }
 ];
 
 // Define account information
 const accountInfo = {
   username: "johnsmith",
   balance: 100,
-  currency: "USD",
+  currency: "USD"
 };
-
-
-
 
 // Define an array of leaderboard entries
 const leaderboardEntries = [
   {
     id: 1,
     username: "JennaSmith",
-    score: 450,
+    score: 450
   },
   {
     id: 2,
     username: "AlexJohnson",
-    score: 320,
+    score: 320
   },
   {
     id: 3,
     username: "ChrisAnderson",
-    score: 280,
+    score: 280
   },
   {
     id: 4,
     username: "LaurenWilliams",
-    score: 210,
+    score: 210
   },
   {
     id: 5,
     username: "SamThompson",
-    score: 180,
+    score: 180
   },
   {
     id: 6,
     username: "RyanClark",
-    score: 150,
+    score: 150
   },
   {
     id: 7,
     username: "GraceMiller",
-    score: 120,
+    score: 120
   },
   {
     id: 8,
     username: "HenryWhite",
-    score: 90,
+    score: 90
   },
   {
     id: 9,
     username: "DaisyWilson",
-    score: 60,
+    score: 60
   },
   {
     id: 10,
     username: "BenJackson",
-    score: 30,
-  },
+    score: 30
+  }
 ];
 
 export default class Index extends Component {
-
-
-
   renderleaderboardEntries() {
     return (
       <div className="leaderboard-entries">
@@ -140,8 +132,6 @@ export default class Index extends Component {
     );
   }
 
-
-
   renderUpcomingEvents() {
     return (
       <div className="upcoming-events">
@@ -150,7 +140,9 @@ export default class Index extends Component {
           <div key={event.id} className="event-item">
             <div className="event-details">
               <div className="event-title">{event.title}</div>
-              <div className="event-time">{event.date} - {event.time}</div>
+              <div className="event-time">
+                {event.date} - {event.time}
+              </div>
             </div>
             <button className="event-bet-button">Bet Now</button>
           </div>
@@ -179,7 +171,9 @@ export default class Index extends Component {
       <div className="account-info">
         <h2>Account Information</h2>
         <p>Username: {accountInfo.username}</p>
-        <p>Balance: {accountInfo.balance} {accountInfo.currency}</p>
+        <p>
+          Balance: {accountInfo.balance} {accountInfo.currency}
+        </p>
       </div>
     );
   }
@@ -190,7 +184,11 @@ export default class Index extends Component {
   };
 
   handleBestRates = () => {
-    this.props.history.push("/best-rates");
+    this.props.history.push("/bestrates");
+  };
+
+  handlehome = () => {
+    this.props.history.push("/index");
   };
 
   render() {
@@ -209,13 +207,16 @@ export default class Index extends Component {
               aria-controls="navbar-side"
               aria-expanded="false"
               aria-label="Open navigation"
-            >
-            </button>
+            />
 
             <div className="collapse navbar-collapse" id="navbar-side">
               <ul className="navbar-nav ml-auto">
                 <li className="button-wrapper">
-                  <button type="button" className="nav-item-home" onClick={console.log("test")}>
+                  <button
+                    type="button"
+                    className="nav-item-home"
+                    onClick={this.handlehome}
+                  >
                     Home
                   </button>
                 </li>
@@ -240,35 +241,36 @@ export default class Index extends Component {
               </ul>
             </div>
           </nav>
-        </div >
-
-          // sections of the page:
-
-        <div className="open-bets-section">
-          {this.renderOpenBets()}
         </div>
-
-        <div className="account-info-section">
-          {this.renderAccountInfo()}
-        </div>
-
+        // sections of the page:
+        <div className="open-bets-section">{this.renderOpenBets()}</div>
+        <div className="account-info-section">{this.renderAccountInfo()}</div>
         <div className="upcoming-events-section">
           {this.renderUpcomingEvents()}
         </div>
-
         <div className="leaderboard-section">
           {this.renderleaderboardEntries()}
         </div>
-
         <div className="buttom-text">
-          At Top Bet, we're committed to promoting responsible and ethical sports betting practices. <br />
-          That's why we offer rewards for watching ads, rather than requiring users to bet money.<br />
-          This ensures that users can enjoy the thrill of sports betting without taking unnecessary risks <br />
-          or engaging in behavior that is harmful to themselves or others.<br />
-          Our rewards program is designed to be fair, transparent, and easy to use. <br />
-          Simply watch ads and earn rewards that you can use to place bets on your favorite sports teams and events.<br />
-          We provide clear and upfront information on the terms and conditions of our rewards program,<br />
-          so you can make informed decisions about your betting and avoid any potential risks or issues.
+          At Top Bet, we're committed to promoting responsible and ethical
+          sports betting practices. <br />
+          That's why we offer rewards for watching ads, rather than requiring
+          users to bet money.
+          <br />
+          This ensures that users can enjoy the thrill of sports betting without
+          taking unnecessary risks <br />
+          or engaging in behavior that is harmful to themselves or others.
+          <br />
+          Our rewards program is designed to be fair, transparent, and easy to
+          use. <br />
+          Simply watch ads and earn rewards that you can use to place bets on
+          your favorite sports teams and events.
+          <br />
+          We provide clear and upfront information on the terms and conditions
+          of our rewards program,
+          <br />
+          so you can make informed decisions about your betting and avoid any
+          potential risks or issues.
         </div>
       </div>
     );
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,194 +1,274 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import api from "../../services/api";
 import $ from "jquery";
 
-import './styles.css';
+import "./styles.css";
 
 export default class Login extends Component {
-    state = {
-        user: "",
-        password: "",
-        changeEmail: "",
+  state = {
+    user: "",
+    password: "",
+    changeEmail: ""
+  };
+
+  componentWillMount() {
+    if (sessionStorage.getItem("username")) this.props.history.push("/main");
+  }
+
+  handleRegistro = () => {
+    this.props.history.push("/registro");
+  };
+
+  handleLogin = async e => {
+    e.preventDefault();
+    const { user, password } = this.state;
+
+    let test = await api.get(`/db`);
+    console.log(test);
+
+    if (!user) {
+      $("#input-login").css("border-color", "blue");
+      setTimeout(function() {
+        $("#input-login").css("border-color", "");
+      }, 3000);
     }
 
-    componentWillMount() {
-        if (sessionStorage.getItem("username")) this.props.history.push('/main');
+    if (!password) {
+      $("#input-password").css("border-color", "blue");
+      setTimeout(function() {
+        $("#input-password").css("border-color", "");
+      }, 3000);
     }
 
-    handleRegistro = () => {
-        this.props.history.push("/registro");
-    }
+    if (!user || !password) return;
+    $("#icon-loading").addClass("fas fa-sync-alt loading-refresh-animate");
 
-    handleLogin = async (e) => {
-        e.preventDefault();
-        const { user, password } = this.state;
+    try {
+      let retriveUsername = await api.get(`/users/username/${user}`);
 
-        let resptest = await api.get(`/`);
-        console.log(resptest);
-
-        if (!user) {
-            $("#input-login").css("border-color", "blue");
-            setTimeout(function () {
-                $("#input-login").css("border-color", "");
-            }, 3000);
-        }
-
-        if (!password) {
-            $("#input-password").css("border-color", "blue");
-            setTimeout(function () {
-                $("#input-password").css("border-color", "");
-            }, 3000);
-        }
-
-        if (!user || !password) return;
-        $("#icon-loading").addClass("fas fa-sync-alt loading-refresh-animate");
-
-        try {
-            let retriveUsername = await api.get(`/users/username/${user}`);
-
-            if (!retriveUsername.data) {
-                // $("#alert-login").addClass("alert alert-danger").text("The username entered is invalid!");
-                $("#icon-loading").removeClass("fas fa-sync-alt loading-refresh-animate");
-                setTimeout(function () {
-                    $("#alert-login").removeClass("alert alert-danger");
-                }, 5000)
-            }
-            else if (password !== retriveUsername.data.password) {
-                // $("#alert-login").addClass("alert alert-danger").text("The password entered is incorrect!");
-                $("#icon-loading").removeClass("fas fa-sync-alt loading-refresh-animate");
-                setTimeout(function () {
-                    $("#alert-login").removeClass("alert alert-danger");
-                }, 5000)
-            } else {
-
-                sessionStorage.setItem("name", retriveUsername.data.name);
-                sessionStorage.setItem("email", retriveUsername.data.email);
-                sessionStorage.setItem("username", retriveUsername.data.username);
-                sessionStorage.setItem("admin", retriveUsername.data.admin);
-                sessionStorage.setItem("loading", true);
-
-                this.props.history.push('/main');
-            }
-        } catch (error) {
-            $("#alert-login").addClass("alert alert-danger").text("An error occurred while requesting your request!");
-            $("#icon-loading").removeClass("fas fa-sync-alt loading-refresh-animate");
-        }
-
-    }
-
-    handleOnChange = (e) => {
-        let name = e.target.name;
-        let value = e.target.value;
-        console.log(name, value)
-
-        this.setState({
-            [name]: value
-        });
-    }
-
-    handleClearEmail = (e) => {
-        this.setState({ changeEmail: "" });
-    }
-
-    handleRecuperarSenha = async (e) => {
-        e.preventDefault();
-
-        let response = await api.get(`/users/email/${this.state.changeEmail}`);
-
-        $("#icon-loading-confirmar").addClass("fas fa-sync-alt loading-refresh-animate");
-
-        if (!response.data.email) {
-            $("#inputEmailChange").css("border-color", "blue");
-            $("#alert-recuperar-senha").addClass("alert alert-danger").text("O e-mail informado não existe!");
-            $("#icon-loading-confirmar").removeClass("fas fa-sync-alt loading-refresh-animate");
-            setTimeout(function () {
-                $("#alert-recuperar-senha").removeClass("alert alert-danger").text("");
-                $("#inputEmailChange").css("border-color", "");
-            }, 3000);
-        } else {
-            let retrivePassword = await api.post(`/recuperar/senha/${this.state.changeEmail}`);
-
-            let sendEmail = await api.get(`/enviar/email/${this.state.changeEmail}/${retrivePassword.data.password}`);
-            $("#alert-recuperar-senha").addClass("alert alert-success").text("The password has been successfully reset, check your email!");
-            $("#icon-loading-confirmar").removeClass("fas fa-sync-alt loading-refresh-animate");
-            this.setState({ changeEmail: "" });
-            $("#inputEmailChange").val(this.state.changeEmail);
-            setTimeout(() => {
-                $("#alert-recuperar-senha").removeClass("alert alert-sucess").text("");
-                $("#inputEmailChange").text("");
-            }, 3000);
-        }
-    }
-
-    render() {
-        return (
-            <div className="div-panel">
-                <div className="wrapper fadeInDown">
-                    <div class="" role="alert" id="alert-login" data-dismiss="alert"></div>
-                    <div id="formContent">
-                        <div id="formHeader">
-                            <h2 className="h2-login active">Login</h2>
-
-                            <h2 className="h2-login inactive underlineHover" onClick={this.handleRegistro}>Register</h2>
-                        </div>
-
-                        <div className="form-padding">
-                            <form>
-                                <input
-                                    type="text"
-                                    id="input-login"
-                                    className="fadeIn second"
-                                    placeholder="Login"
-                                    name="user"
-                                    onChange={(event) => this.handleOnChange(event)}
-                                    required={true}
-                                />
-                                <input
-                                    type="password"
-                                    id="input-password"
-                                    className="fadeIn third"
-                                    placeholder="Password"
-                                    name="password"
-                                    onChange={(event) => this.handleOnChange(event)}
-                                    required
-                                />
-                                <button type="submit" className="fadeIn fourth btn-login" value="Enter" onClick={this.handleLogin} >Enter &nbsp;<i className="" id="icon-loading"></i></button>
-                            </form>
-                        </div>
-
-                        <div id="formFooter">
-                            <a className="underlineHover link-pointer" data-toggle="modal" data-target="#modalRecuperarSenha">Forgot password?</a>
-                        </div>
-
-                    </div>
-                </div>
-
-                <div class="modal fade" id="modalRecuperarSenha" role="dialog" aria-labelledby="modalRecuperarSenhaLabel" aria-hidden="true">
-                    <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="modalRecuperarSenhaLabel">Enter your email and password below</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <form>
-                                <div className="modal-body">
-                                    <div class="" role="alert" id="alert-recuperar-senha" data-dismiss="alert"></div>
-                                    <input id="inputEmailChange" class="w-100 mx-auto" type="email" placeholder="E-mail" name="changeEmail" onChange={this.handleOnChange} />
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-dismiss="modal" onClick={this.handleClearEmail}>Cancel</button>
-                                    <button type="submit" class="btn btn-primary" id="btn-confimarRecuperar" onClick={this.handleRecuperarSenha}>Confirm &nbsp;<i className="" id="icon-loading-confirmar"></i></button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
+      if (!retriveUsername.data) {
+        // $("#alert-login").addClass("alert alert-danger").text("The username entered is invalid!");
+        $("#icon-loading").removeClass(
+          "fas fa-sync-alt loading-refresh-animate"
         );
+        setTimeout(function() {
+          $("#alert-login").removeClass("alert alert-danger");
+        }, 5000);
+      } else if (password !== retriveUsername.data.password) {
+        // $("#alert-login").addClass("alert alert-danger").text("The password entered is incorrect!");
+        $("#icon-loading").removeClass(
+          "fas fa-sync-alt loading-refresh-animate"
+        );
+        setTimeout(function() {
+          $("#alert-login").removeClass("alert alert-danger");
+        }, 5000);
+      } else {
+        sessionStorage.setItem("name", retriveUsername.data.name);
+        sessionStorage.setItem("email", retriveUsername.data.email);
+        sessionStorage.setItem("username", retriveUsername.data.username);
+        sessionStorage.setItem("admin", retriveUsername.data.admin);
+        sessionStorage.setItem("loading", true);
+
+        this.props.history.push("/main");
+      }
+    } catch (error) {
+      $("#alert-login")
+        .addClass("alert alert-danger")
+        .text("An error occurred while requesting your request!");
+      $("#icon-loading").removeClass("fas fa-sync-alt loading-refresh-animate");
     }
+  };
+
+  handleOnChange = e => {
+    let name = e.target.name;
+    let value = e.target.value;
+    console.log(name, value);
+
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleClearEmail = e => {
+    this.setState({ changeEmail: "" });
+  };
+
+  handleRecuperarSenha = async e => {
+    e.preventDefault();
+
+    let response = await api.get(`/users/email/${this.state.changeEmail}`);
+
+    $("#icon-loading-confirmar").addClass(
+      "fas fa-sync-alt loading-refresh-animate"
+    );
+
+    if (!response.data.email) {
+      $("#inputEmailChange").css("border-color", "blue");
+      $("#alert-recuperar-senha")
+        .addClass("alert alert-danger")
+        .text("O e-mail informado não existe!");
+      $("#icon-loading-confirmar").removeClass(
+        "fas fa-sync-alt loading-refresh-animate"
+      );
+      setTimeout(function() {
+        $("#alert-recuperar-senha")
+          .removeClass("alert alert-danger")
+          .text("");
+        $("#inputEmailChange").css("border-color", "");
+      }, 3000);
+    } else {
+      let retrivePassword = await api.post(
+        `/recuperar/senha/${this.state.changeEmail}`
+      );
+
+      let sendEmail = await api.get(
+        `/enviar/email/${this.state.changeEmail}/${
+          retrivePassword.data.password
+        }`
+      );
+      $("#alert-recuperar-senha")
+        .addClass("alert alert-success")
+        .text("The password has been successfully reset, check your email!");
+      $("#icon-loading-confirmar").removeClass(
+        "fas fa-sync-alt loading-refresh-animate"
+      );
+      this.setState({ changeEmail: "" });
+      $("#inputEmailChange").val(this.state.changeEmail);
+      setTimeout(() => {
+        $("#alert-recuperar-senha")
+          .removeClass("alert alert-sucess")
+          .text("");
+        $("#inputEmailChange").text("");
+      }, 3000);
+    }
+  };
+
+  render() {
+    return (
+      <div className="div-panel">
+        <div className="wrapper fadeInDown">
+          <div class="" role="alert" id="alert-login" data-dismiss="alert" />
+          <div id="formContent">
+            <div id="formHeader">
+              <h2 className="h2-login active">Login</h2>
+
+              <h2
+                className="h2-login inactive underlineHover"
+                onClick={this.handleRegistro}
+              >
+                Register
+              </h2>
+            </div>
+
+            <div className="form-padding">
+              <form>
+                <input
+                  type="text"
+                  id="input-login"
+                  className="fadeIn second"
+                  placeholder="Login"
+                  name="user"
+                  onChange={event => this.handleOnChange(event)}
+                  required={true}
+                />
+                <input
+                  type="password"
+                  id="input-password"
+                  className="fadeIn third"
+                  placeholder="Password"
+                  name="password"
+                  onChange={event => this.handleOnChange(event)}
+                  required
+                />
+                <button
+                  type="submit"
+                  className="fadeIn fourth btn-login"
+                  value="Enter"
+                  onClick={this.handleLogin}
+                >
+                  Enter &nbsp;
+                  <i className="" id="icon-loading" />
+                </button>
+              </form>
+            </div>
+
+            <div id="formFooter">
+              <a
+                className="underlineHover link-pointer"
+                data-toggle="modal"
+                data-target="#modalRecuperarSenha"
+              >
+                Forgot password?
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div
+          class="modal fade"
+          id="modalRecuperarSenha"
+          role="dialog"
+          aria-labelledby="modalRecuperarSenhaLabel"
+          aria-hidden="true"
+        >
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="modalRecuperarSenhaLabel">
+                  Enter your email and password below
+                </h5>
+                <button
+                  type="button"
+                  class="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <form>
+                <div className="modal-body">
+                  <div
+                    class=""
+                    role="alert"
+                    id="alert-recuperar-senha"
+                    data-dismiss="alert"
+                  />
+                  <input
+                    id="inputEmailChange"
+                    class="w-100 mx-auto"
+                    type="email"
+                    placeholder="E-mail"
+                    name="changeEmail"
+                    onChange={this.handleOnChange}
+                  />
+                </div>
+                <div class="modal-footer">
+                  <button
+                    type="button"
+                    class="btn btn-secondary"
+                    data-dismiss="modal"
+                    onClick={this.handleClearEmail}
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    type="submit"
+                    class="btn btn-primary"
+                    id="btn-confimarRecuperar"
+                    onClick={this.handleRecuperarSenha}
+                  >
+                    Confirm &nbsp;
+                    <i className="" id="icon-loading-confirmar" />
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
