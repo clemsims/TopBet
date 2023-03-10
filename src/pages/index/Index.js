eed9f0ebebd6c66a16a6a94820b59033027ac9f9
antoine_bossan <antoine9589@gmail.com>
2023-03-10 07:10:32 +0000

    start to change color style

--- src/pages/index/Index.js
+++ src/pages/index/Index.js
@@ -32,7 +32,7 @@ export default class Index extends Component {
   renderUpcomingEvents() {
     return (
       <div className="upcoming-events">
-        <h2>Upcoming Events</h2>
+        <h2 id="upcoming_event_title"> Upcoming Events </h2>
         {upcomingEvents.map(event => (
           <div key={event.id} className="event-item">
             <div className="event-details">
@@ -115,26 +115,28 @@ export default class Index extends Component {
           <div className="grid-container">
             <div className="upcoming-events-section"> {this.renderUpcomingEvents()} </div>
             <div className="leaderboard-section"> {this.renderleaderboardEntries()} </div>
-            <div className="buttom-text">
-              At Top Bet, we're committed to promoting responsible and ethical
-              sports betting practices. <br />
-              That's why we offer rewards for watching ads, rather than requiring
-              users to bet money.
-              <br />
-              This ensures that users can enjoy the thrill of sports betting without
-              taking unnecessary risks <br />
-              or engaging in behavior that is harmful to themselves or others.
-              <br />
-              Our rewards program is designed to be fair, transparent, and easy to
-              use. <br />
-              Simply watch ads and earn rewards that you can use to place bets on
-              your favorite sports teams and events.
-              <br />
-              We provide clear and upfront information on the terms and conditions
-              of our rewards program,
-              <br />
-              so you can make informed decisions about your betting and avoid any
-              potential risks or issues.
+            <div className="footer">
+              <div className="buttom-text">
+                At Top Bet, we're committed to promoting responsible and ethical
+                sports betting practices. <br />
+                That's why we offer rewards for watching ads, rather than requiring
+                users to bet money.
+                <br />
+                This ensures that users can enjoy the thrill of sports betting without
+                taking unnecessary risks <br />
+                or engaging in behavior that is harmful to themselves or others.
+                <br />
+                Our rewards program is designed to be fair, transparent, and easy to
+                use. <br />
+                Simply watch ads and earn rewards that you can use to place bets on
+                your favorite sports teams and events.
+                <br />
+                We provide clear and upfront information on the terms and conditions
+                of our rewards program,
+                <br />
+                so you can make informed decisions about your betting and avoid any
+                potential risks or issues.
+              </div>
             </div>
           </div>
         </div>
