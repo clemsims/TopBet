66a61b1442b4381bdac0d04ee829a2cf96430718
antoine_bossan <antoine9589@gmail.com>
2023-03-12 09:23:41 +0000

    make header before login

--- src/components/header_before_login/Header_before_login.js
+++ src/components/header_before_login/Header_before_login.js
@@ -1,24 +1,8 @@
 import React, { Component } from 'react';
-
 import { withRouter } from "react-router";
-
-import $ from "jquery";
-
+import "./styles.css";
 
 class Header_no_login extends Component {
-    state = {
-        ranking: "",
-        home: "",
-        admin: false,
-        changePassword: "",
-        changeNewPassword: ""
-    };
-
-    async componentWillMount() {
-
-        this.setState({ admin: true });
-        $("#admin").removeClass("invisible");
-    }
 
     handleLogin = () => {
         if (sessionStorage.getItem("username")) this.props.history.push("/main");
@@ -33,55 +17,38 @@ class Header_no_login extends Component {
         this.props.history.push("/");
     };
 
-
     render() {
         return (
             <div>
-                <div className="headertext">
-                    <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-                        <a className="navbar-brand h4 mb-0" href="#">
-                            <button className="logo" type="button" onClick={this.handlehome}
-                            >TopBet
-
-                            </button>
-
-                        </a>
-                        <button
-                            className="navbar-toggler"
-                            type="button"
-                            data-toggle="collapse"
-                            data-target="#navbar-side"
-                            aria-controls="navbar-side"
-                            aria-expanded="false"
-                            aria-label="Open navigation"
-                        />
-
-                        <div className="collapse navbar-collapse" id="navbar-side">
-                            <ul className="navbar-nav ml-auto">
-                                <li className="button-wrapper">
-                                </li>
-                                <li>
-                                    <button
-                                        type="button"
-                                        className="nav-item-betnow"
-                                        onClick={this.handleLogin}
-                                    >
-                                        Bet now (Login)
-                                    </button>
-                                </li>
-                                <li>
-                                    <button
-                                        type="button"
-                                        className="nav-item-bestrates"
-                                        onClick={this.handleBestRates}
-                                    >
-                                        Best Rates
-                                    </button>
-                                </li>
-                            </ul>
-                        </div>
-                    </nav>
-                </div>
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+                    <button className="logo" type="button" onClick={this.handlehome}
+                    >TopBet
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbar-side">
+                        <ul className="navbar-nav ml-auto">
+                            <li className="button-wrapper">
+                            </li>
+                            <li>
+                                <button
+                                    type="button"
+                                    className="nav-item-betnow"
+                                    onClick={this.handleLogin}
+                                >
+                                    Bet now (Login)
+                                </button>
+                            </li>
+                            <li>
+                                <button
+                                    type="button"
+                                    className="nav-item-bestrates"
+                                    onClick={this.handleBestRates}
+                                >
+                                    Best Rates
+                                </button>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
             </div>
         );
     }
