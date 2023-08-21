8cc831fbd5c2622864be3f1256cbbf9df11643fb
Mark Piquant <91563126+markpiquant@users.noreply.github.com>
2023-08-21 16:02:31 +0000

    modified:   src/components/comp_Card/card.js 	modified:   src/components/comp_Card/style.css     modified:   src/pages/customize/Customize.js 	modified:   src/pages/customize/style.css

--- src/components/comp_Card/card.js
+++ src/components/comp_Card/card.js
@@ -3,7 +3,24 @@ import './style.css';
 
 
 class Card extends React.Component {
-  
+  handleNotInterestedClick = () => {
+    // Appeler la fonction passée en prop pour indiquer un désintérêt
+    this.props.handleInterest(this.props.sportName, 'Not Interested');
+    // Masquer la card
+    this.props.hideCard();
+  };
+
+  handleInterestedClick = () => {
+    // Appeler la fonction passée en prop pour indiquer un intérêt
+    this.props.handleInterest(this.props.sportName, 'Interested');
+    // Masquer la card
+    this.props.hideCard();
+  };
+
+  handleHideClick = () => {
+    // Appeler la fonction passée en prop pour masquer la card
+    this.props.hideCard();
+  };
     render() {
       const { src, sportName } = this.props;
       
@@ -16,8 +33,8 @@ class Card extends React.Component {
             </div>
             <div className="card-content">
               <div className="sport-actions">
-                <button className="not-interested-button">Not Interested</button>
-                <button className="interested-button">Interested</button> 
+                <button onClick={this.handleNotInterestedClick} className="not-interested-button">Not Interested</button>
+                <button onClick={this.handleInterestedClick} className="interested-button">Interested</button> 
               </div>
               <div className="swipe">
                 Swipe left or right
--- src/pages/customize/Customize.js
+++ src/pages/customize/Customize.js
@@ -9,37 +9,65 @@ export default class Customize extends Component {
     let name = e.target.name;
     let value = e.target.value;
     // Check if the name is 'DD/MM' and validate the date format
-    if (name =e== 'DD/MM' && !/^\d{2}\/\d{2}$/.test(value)) {
-      this.setState({
-          errorMessage: 'Please enter a valid date in DD/MM format.'
-      });
-    } else {
-        this.setState({ errorMessage: '' }); // Reset error message if the date is valid or for other inputs
-    }
     this.setState({
       [name]: value
     });
   };
   
   state = {
-    isDropdownVisible: {
-      team1: false,
-      team2: false,
-      date: false
+    Boxe: {
+      show:true,
+      interested:'DNK'
     },
-    errorMessage: ''
+    Football: {
+      show:true,
+      interested:'DNK'
+    },
+    showOverlay: true
   };
   
-
-
+  // Méthode pour masquer la card
+  handleInterest = (sport, value) => {
+    this.setState(prevState => ({
+      [sport]: {
+        ...prevState[sport],
+        interested: value
+      }
+    }));
+  };
+  handleOverlayClick = () => {
+    this.setState({ showOverlay: false });
+  };
+  // Méthode pour masquer la card
+  hideSport = (sport) => {
+    this.setState(prevState => ({
+      [sport]: {
+        ...prevState[sport],
+        show: false
+      }
+    }));
+  };
   render() {
     return (
       <div className="div-panel">
+        {this.state.showOverlay && (
+        <div className="overlay-major">
+          <div className="overlay-major-content">
+            <p>
+              Vous allez choisir les sports que vous suivez.
+            </p>
+            <button className="overlay-major-button" onClick={this.handleOverlayClick}>
+              Allons-y !
+            </button>
+          </div>
+        </div>
+        )}
         <div className="headertext">
           <Navbar history={this.props.history}  />
         </div>
         <div className="card-container">
-          <Card src="/images/Boxe.png" sportName="Boxe" />
+        { this.state.Boxe.show && <Card src="/images/Boxe.png" sportName="Boxe" hideCard={() => this.hideSport('Boxe')} handleInterest={this.handleInterest} /> }
+        { this.state.Football.show && <Card src="/images/Football.png" sportName="Football" hideCard={() => this.hideSport('Football')} handleInterest={this.handleInterest} /> }
         </div>
       </div>
     );
